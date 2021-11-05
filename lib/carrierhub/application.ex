50705cd9511c30720d24711caf3cf5ec355c2ea4
Felipe José <lipejose@icloud.com>
2021-11-05 09:09:39 -0300

    feat: adjust app ex

--- lib/carrierhub/application.ex
+++ lib/carrierhub/application.ex
@@ -1,33 +1,21 @@
 defmodule Carrierhub.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
-  @moduledoc false
 
   use Application
 
   @impl true
   def start(_type, _args) do
-    import Supervisor.Spec
     children = [
-      # Start the Telemetry supervisor
       CarrierhubWeb.Telemetry,
-      # Start the PubSub system
       {Phoenix.PubSub, name: Carrierhub.PubSub},
-      # Start the Endpoint (http/https)
       CarrierhubWeb.Endpoint,
-      worker(Mongo, [[name: :mongo, url: "mongodb://localhost:27017/carrierhub_local", pool_size: 10]])
-      # Start a worker by calling: Carrierhub.Worker.start_link(arg)
-      # {Carrierhub.Worker, arg}
+      {Mongo, [name: :mongo, url: "mongodb://localhost:27017/carrierhub_local", pool_size: 10]}
+
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Carrierhub.Supervisor]
     Supervisor.start_link(children, opts)
   end
 
-  # Tell Phoenix to update the endpoint configuration
-  # whenever the application is updated.
   @impl true
   def config_change(changed, _new, removed) do
     CarrierhubWeb.Endpoint.config_change(changed, removed)
