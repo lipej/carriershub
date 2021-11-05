4cd5cdf817296437d1a87b3b60d049b113a03a1c
Felipe José <lipejose@icloud.com>
2021-11-05 09:10:02 -0300

    feat: faker carrier plugin

---
+++ lib/carrierhub/carriers/plugins/faker.ex
@@ -0,0 +1,13 @@
+defmodule Carrierhub.Carriers.Plugins.Faker do
+  
+  def tracking(data, fields) do
+    IO.inspect("performing tracking action from Faker")
+    IO.inspect({data, fields})
+  end
+
+  def solicitation(data, fields) do
+    IO.inspect("performing solicitation action from Faker")
+    IO.inspect({data, fields})
+  end 
+
+end
