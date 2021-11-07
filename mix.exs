76f0d862df8ecf18634a6745d1870b79bc0f421f
Felipe José <lipejose@icloud.com>
2021-11-07 12:41:57 -0300

    feat: enable os metrics

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Carrierhub.MixProject do
   def application do
     [
       mod: {Carrierhub.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
