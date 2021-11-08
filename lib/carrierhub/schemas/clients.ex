fe2313e94c804a4d4e3467a8c210b8a39bc24550
Felipe José <lipejose@icloud.com>
2021-11-08 15:20:23 -0300

    feat: update migration & schema

--- lib/carrierhub/schemas/clients.ex
+++ lib/carrierhub/schemas/clients.ex
@@ -1,15 +1,25 @@
 defmodule Carrierhub.Schema.Clients do
   use Ecto.Schema
   alias Carrierhub.Schema.{Integrations}
+  import Ecto.Changeset
 
   @primary_key {:id, :binary_id, autogenerate: true}
+  @fields [:name, :key, :cnpj]
 
   schema "clients" do
    field :name, :string
    field :key, :string
+   field :cnpj, :string
    has_many :integrations, Integrations
    timestamps()
   end
 
-  
+  def changeset(params) do
+    %__MODULE__{}
+    |> cast(params, @fields)
+    |> validate_required(@fields)
+    |> validate_length(:cnpj, is: 14)
+    |> unique_constraint(:cnpj)
+  end
+    
 end
--- lib/carrierhub/schemas/integrations.ex
+++ lib/carrierhub/schemas/integrations.ex
@@ -1,9 +1,11 @@
 defmodule Carrierhub.Schema.Integrations do
   use Ecto.Schema
   alias Carrierhub.Schema.{Clients}
+  import Ecto.Changeset
 
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
+  @fields [:name, :fields, :clients_id]
 
   schema "integrations" do
    field :name, :string
@@ -11,4 +13,11 @@ defmodule Carrierhub.Schema.Integrations do
    belongs_to :clients, Clients
    timestamps()
   end
+
+  def changeset(params) do
+    %__MODULE__{}
+    |> cast(params, @fields)
+    |> validate_required(@fields)
+  end
+
 end
