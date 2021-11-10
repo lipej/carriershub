348d93129b1ebf67ce4f05151d0d600aeb5db397
Felipe José <lipejose@icloud.com>
2021-11-09 21:07:37 -0300

    refactor: adjust loader

--- lib/carrierhub/carriers/loader.ex
+++ lib/carrierhub/carriers/loader.ex
@@ -1,21 +1,14 @@
 defmodule Carrierhub.Carriers.Loader do
   alias Carrierhub.Carriers.Plugins
 
-  def pluginLoader(name) do
-      plugin_name = Module.concat(Plugins, String.capitalize(name))
+  def plugin_loader(name) do
+    plugin_name = Module.concat(Plugins, String.capitalize(name))
 
-      if canHandler(Code.ensure_loaded(plugin_name)),
-        do: {:ok, plugin_name},
-        else: {:error, %{result: "#{name} plugin was not implemeted", status: :bad_request} }
-  end
-
-  def canPerformAction(plugin, action) do
-    if Keyword.has_key?(plugin.__info__(:functions), String.to_atom(action) ),
-      do: {:ok, {String.to_atom(action)}},
-      else: {:error, %{result: "Can't perform action: #{action}", status: :bad_request} } 
+    if canHandler(Code.ensure_loaded(plugin_name)),
+      do: {:ok, plugin_name},
+      else: {:error, %{result: "#{name} plugin was not implemeted", status: :bad_request}}
   end
 
   defp canHandler({:module, _module}), do: true
   defp canHandler({:error, _message}), do: false
-  
 end
